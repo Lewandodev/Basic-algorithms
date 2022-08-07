b91ce36f0ee93396df6b8bcdd0833bed8265646f
Lewandodev <mlewandowski380@gmail.com>
2022-08-07 23:48:06 +0200

    Selection sort with my own comments in English/Polish

--- Selection sort.py
+++ Selection sort.py
@@ -34,18 +34,27 @@ print('ordered:',selection_sort(new_array))
 
 def sortowanie_przez_wybieranie(tablica):
 
-    for i in range(0,len(tablica)-1):
-        minIndex=i
+    #będziemy potrzebowali dwóch pętli pierwszej "zewnętrznej" szukającej najmniejszego elementu
+    #oraz drugiej "wewnętrznej" szukającej czy nie ma elementu po pierwszym minimum, które jest mniejsze
+
+    for i in range(0,len(tablica)-1): #ostatni element pomijamy ponieważ nie będziemy go mieli z czym porównywać
+
+        minIndex=i #aktualna minimalna wartość
+
         for j in range(i+1,len(tablica)):
             if tablica[minIndex]>tablica[j]:
+                # druge pętla przechodzi od index i minimalnej wartości dalej na prawo sprawdzając czy nie ma nic mniejszego
                 minIndex=j
+                # jeśli jest minimum jest nadpisywane
 
         '''pom=tablica[minIndex]
-        tablica[minIndex]=tablica[i]
+        tablica[minIndex]=tablica[i] 
         tablica[i]=pom'''
+        # alternatywna wersja zamiany elementów ze zmienną pomocniczą
 
-        tablica[i],tablica[minIndex]=tablica[minIndex],tablica[i]
-    return tablica
+
+        tablica[i],tablica[minIndex]=tablica[minIndex],tablica[i] #zamiana elementów w specjalnej notacji
+    return tablica                                              #element tablicy z indeksem i zamieniamy z elementem o indeksie minIndex i na odwrót
 
 print(sortowanie_przez_wybieranie([5,8,3,2,7,9,1,4]))
 
