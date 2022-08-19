d1a9843e7d873862923e192c8d49cd1b0ef1e459
Lewandodev <mlewandowski380@gmail.com>
2022-08-19 23:59:31 +0200

    Basic form decimal conversion first update

--- Basic from decimal conversion.py
+++ Basic from decimal conversion.py
@@ -1,3 +1,17 @@
+#basic conversion from decimal to systems up to 9
+
+def decimal_upTo9_conv(cipher,system):
+    inverted='' #this variable stores inverted rest divisions of user inserted cipher
+    n=cipher
+    while n!=0:
+        r=n//system
+        c=n%system
+        c=str(c)  #we need to convert the value to string to avoid errors
+        odwr=c+odwr
+        n=r
+    return odwr
+
+print('Converting values up to 9 conversion of 166 to 4 system:',decimal_upTo9_conv(166,4))
 def decymalny_na_binarny(cyfra):
     odwr='' # ta zmienna przechowuje czytane od tyłu reszty z dzielenia wprowadzonej przez uzytkowniaka cyfyr przez 2
     sys=2 #bo binarny dzielimy przez 2 w innych systemach może być 4 3 8 itd
@@ -5,7 +19,7 @@ def decymalny_na_binarny(cyfra):
     while n!=0:
         r=n//sys
         c=n%sys
-        c=str(c)  #konwertujemy na steringa wartość żeby nei wywaliło błedu
+        c=str(c)  #konwertujemy na stringa wartość żeby nie wywalało błedów
         odwr=c+odwr
         n=r
     return odwr
@@ -27,3 +41,14 @@ def decymalny_do_9(cyfra,sys):
 print(decymalny_do_9(15,9))
 
 print(decymalny_do_9(17,6))
+
+
+
+
+
+
+
+
+
+#additional note: as I'm now back from vacation I decided to start revising some of the algorithm projects that I was unable to edit during my holidays
+#the extensions of all the files will be changed from .txt to .py with additional new code and commentary
