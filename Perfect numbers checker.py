225816ceb7444f74f9dc342597a2bca6314c1952
Lewandodev <mlewandowski380@gmail.com>
2022-08-21 21:47:28 +0200

    Perfect numbers checker/searcher algorithm with my own Polish and English commentary

--- Perfect numbers checker.py
+++ Perfect numbers checker.py
@@ -36,7 +36,7 @@ print('liczby doskonałe w zakresie do 100:',znajdź_liczby_doskonałe(100))
 
 print('\nZadanie z znajdywania liczb doskonałych w zakresie zoptymalizowane')
 
-#Zad 3
+
 def znajdź_liczby_doskonałe_zad3(zakres):
     lista_doskonałych=[]
     for cyfry in range(6,zakres+1): #lub range to (6,zakres+1,2) wtedy nie sprawdzamy czy dzieli się bez reszty przez 2
@@ -46,7 +46,7 @@ def znajdź_liczby_doskonałe_zad3(zakres):
         else:
             continue
     return lista_doskonałych
-print('liczby doskonałe w zakresie do 1000:',znajdź_liczby_doskonałe_zad3(1000))   #orgyinalnie 10000
+print('liczby doskonałe w zakresie do 10000:',znajdź_liczby_doskonałe_zad3(10000))
 
 print('\nChecking if number is perfect')
 
@@ -77,10 +77,10 @@ print('Is 8128 perfect?:',czy_doskonała(8128))
 print('\nExercise searching for perfect numbers in range:')
 
 
-def find_perfect_numbs(zakres):
+def find_perfect_numbs(rangee):
     list_perfect=[]
 
-    for numbs in range(1,zakres+1):
+    for numbs in range(1,rangee+1):
         if is_perfect(numbs)==True:
             list_perfect.append(numbs)
     return list_perfect
@@ -89,3 +89,17 @@ print('perfect numbers in range from 1 to 100:',find_perfect_numbs(100))
 
 print('\nExercise searching for perfect numbers in range optimized')
 
+
+def find_perfnumbs_opt(rangee):
+    list_of_perfs=[]
+    for numbs in range(6,rangee+1): #or we use range(6,range+1,2) with step (2) in order to don't use the if function
+        if numbs%2==0:    #in optimized version we skip all the odd numbers fastening up our running time
+            if is_perfect(numbs)==True:
+                list_of_perfs.append(numbs)
+        else:
+            continue
+    return list_of_perfs
+print('perfect numbers in range up to 10000:',find_perfnumbs_opt(10000))
+
+
+#algorithm revised
