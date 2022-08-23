eb72feac70b56f8175f2ee8fa03830216bc23b09
Lewandodev <mlewandowski380@gmail.com>
2022-08-24 00:01:38 +0200

    First revision with minor improvements

--- Simple recursion.py
+++ Simple recursion.py
@@ -1,5 +1,8 @@
 #ALGORYTMY REKURENCYJNE
+#RECURSIVE ALGORITHMS
 
+#Recursion example
+#przykład rekurencji
 def rekur(n):
     if n%2==0:
         if n ==2:
@@ -20,24 +23,20 @@ def suma_nieparzystych(n):
             return suma_nieparzystych(n-1)+n
     else:
         return suma_nieparzystych(n-1)
-print(suma_nieparzystych(100))
-print('---------------------')
+print('Suma liczb nieparzystych do 100',suma_nieparzystych(100))
+
 #Zad 2
+#suma pięciu liczb rekruencyjnie
 def sumapieciu(limit,liczba):
     if limit==5:
         return liczba
     return sumapieciu(limit+1,liczba*liczba)+liczba
-print(sumapieciu(1,2))
-print('----------------------')
-#Zad 3
-
-
-
-
-
+print('\nSuma pięciu liczb:',sumapieciu(1,2))
 
 
 #Rekurencja średniozaawansowane przypadki
+#silnia jest jednym z najprostszych przykładów rekurencji
+
 def zwroc_silnie(n):
     if n==1 or n==0:
         return 1
@@ -46,7 +45,8 @@ def zwroc_silnie(n):
 print(zwroc_silnie(6))
 print('--------------------')
 
-
+#algorytm fibonacciego można rozwiązać z użyciem metod rekurencyjnych bądź iteracyjnych
+#rekurencja w algorytmach fibonacciego jest jednak nieefektowna czasowo i musi zostać poddana memoizacji
 def fibonacii(n):
     if n==1 or n==2:
         return 1
@@ -54,3 +54,9 @@ def fibonacii(n):
         return fibonacii(n-1)+fibonacii(n-2)
 print(fibonacii(6))
 
+#innym przykładem rekurencji jest algorytm ekulidesa
+
+def NWD_rekurencyjne(a,b):
+    if b!=0:
+        return NWD_rekurencyjne(b,a%b)
+    return a
