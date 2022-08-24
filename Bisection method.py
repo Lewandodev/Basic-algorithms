a5b8c941b46b73081d83a7e4562f2de839df6210
Lewandodev <mlewandowski380@gmail.com>
2022-08-24 23:40:23 +0200

    First revision of Bisection method

--- Bisection method.py
+++ Bisection method.py
@@ -1,15 +1,29 @@
-#marked for recision after I come from vacation 
+#Bisection method
+#Metoda połowienia
+
+#Bisection method is used for finding a root of a continuous function in numerical interval
+#Metoda Połowienia Przedziałów służy do znajdowania miejsca zerowego pewnej ciągłej funkcji f ograniczonej do pewnego przedziału
 
 #dla funkcji f=x+2
+#for f=x+2 math function
+
+
+
+#our continuous function is defined as following: y=ax+b with epsilon being precision value
+#funkcja liniowa/ciągła jest definiowana jako: y=ax+b, epsilon odpowiada za skalę dokładności wyznaczonego miejsca zerowego
 
 def wartosć_funkcji(x):
     return x+2
+    #in this function we declare Value of function
+    #w tej funkcji deklarujemy naszą funkcję liniową
 
 def połowienie_przedziałów(a,b,epsilon):
     if wartosć_funkcji(a)==0:
         return a
     if wartosć_funkcji(b)==0:
         return b
+    #if either a or b equals 0 our root is already found as it is a or b
+    #w przypadku gdy a lub b wynosi 0 nasze miejsce zerowe jest jedną z tych zmiennych
 
     while b-a>epsilon:
         środek=(a+b)/2
@@ -23,25 +37,16 @@ def połowienie_przedziałów(a,b,epsilon):
 
 
 #ZADANIA PONIŻEJ
-
-
-
-
-
-
+#Exercises
 
 
 import math
 
 
 
-
-
-
-
-
-
 #ZADANIE 1
+#EXERCISE 1
+
 def w_FUNKCJA(x):
     return x**2-4*x-6
 
@@ -62,6 +67,8 @@ print('Zadanie 1: ')
 print(round(Z1połowienie(0,20,0.001),4))
 
 #ZADANIE 2
+#EXERCISE 2
+
 def w2_funkcji(x):
     return 2*(x**3)+2
 
