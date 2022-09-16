ab229037b7fb0483bf8fde3d3746776d5e5006e1
Lewandodev <mlewandowski380@gmail.com>
2022-09-16 23:59:33 +0200

    Simple greedy algorithm

--- Greedy algorithm.py
+++ Greedy algorithm.py
@@ -5,7 +5,8 @@
 #Algorytm zachłanny  – algorytm, który w celu wyznaczenia rozwiązania w każdym kroku dokonuje zachłannego, tj. najlepiej rokującego w danym momencie wyboru rozwiązania
 
 
-#Greedy algorithm returning the cahnge from multiple coins
+#Greedy algorithm returning the change from multiple coins
+#for example: 7 can be returned in 2 coins
 
 def ile_monet(reszta):
     monety=[1,2,5]
@@ -23,3 +24,23 @@ def ile_monet(reszta):
     return liczba_moent_do_wydania
 
 print('z ilu monet wydać możemy 7',ile_monet(7))
+
+
+
+#exercise
+
+def transoprt_mąki(ilość_mąki):
+    pojeność_opakowań=[15,5,2,1]
+    liczba_opakowań=0
+
+    while ilość_mąki>0:
+        odejmowana_masa=0
+        for opakowania in pojeność_opakowań:
+            if opakowania<=ilość_mąki and opakowania>odejmowana_masa:
+                odejmowana_masa=opakowania
+
+        ilość_mąki-=odejmowana_masa
+        liczba_opakowań+=1
+    return liczba_opakowań
+
+print(transoprt_mąki(68))
