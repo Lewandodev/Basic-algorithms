351c71c3d961b5cdfa01beddbf2db43e14dc7bca
Lewandodev <mlewandowski380@gmail.com>
2022-09-21 23:43:17 +0200

    Caesar cipher algorithm/translator can both encrypt and decrypt This caesar cipher doesn't detect if something is encrypted in Caesar cipher

---
+++ Caesar Cipher.py
@@ -0,0 +1,29 @@
+#One of the oldest substitution ciphers algorithm one of the simplest and most widely known encryption techniques
+#each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet
+#for example.: with a right shift of 3, A would be replaced by D
+
+def encrypt(w,k):  #w- the value we input, k- the key we will be shifting with
+    k=k%26
+    cipher=''
+    for i in range(0,len(w)):
+        letter=ord(w[i])
+        if letter+k>90:     #90 is the value of letter Z in ASCII
+            cipher+=chr(letter+k-26)
+        else:
+            cipher+=chr(letter+k) #we modify the ASCII value of letter by adding our key value thus shofting the value
+    return cipher
+
+def decrypt(w,k):
+    k=k%26
+    decryption=''
+    for i in range(0,len(w)):
+        letter=ord(w[i])
+        if letter-k<65:
+            decryption=decryption+chr(letter-k+26)
+        else:
+            decryption+=chr(letter-k)
+    return decryption
+
+
+
+
